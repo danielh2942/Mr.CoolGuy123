ea7604c4ef8405692fc67b4f2646f3673abaf349
Daniel Hannon <12559951+danielh2942@users.noreply.github.com>
2019-11-19 11:02:41 +0000

    Update and rename timeSinceThatcher.js to timesincethatcher.js
    renamed to fit conventions and made few corrections

---
+++ commands/timesincethatcher.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'timesincethatcher',
+    description: 'prints days since thatcher\'s death in days, and an image of her grave',
+    
+	execute(message, args) {
+		let timeOfDeath = Date.parse('2013/04/08');
+		let timeNow = Date.now();
+		const difference = timeNow -  timeOfDeath;
+		let numOfDays = 0;
+
+		if (args[0] == "--thatcher") {
+    		numOfDays = Math.floor(difference / 8.64e+7);
+    		message.channel.send("It has been" +numOfDays+ "days since Thatcher has died");
+    		message.channel.send("Here's a picture of Thatcher's grave", {files: ["https://upload.wikimedia.org/wikipedia/commons/e/e3/MTgrave2.jpeg"]});
+		}
+    },
+};
+
