a94956ccd7defef5e61d76a18c48842cb34a36d7
[nmq-hyt] <conoroboyle83@gmail.com>
2019-11-19 00:18:53 +0000

    changed thacher to use args[0] instead

--- commands/timeSinceThatcher.js
+++ commands/timeSinceThatcher.js
@@ -2,17 +2,16 @@ module.exports = {
     name: 'timeSinceThatcher',
     description: 'prints days since thatcher\'s death in days, and an image of her grave',
     
- execute(message, args) {
+execute(message, args) {
 let timeOfDeath = Date.parse('2013/04/08');
 let timeNow = Date.now();
 const difference = timeNow -  timeOfDeath;
 let numOfDays = 0;
 
-if (message == "-thatcher") {
-numOfDays = Math.floor(difference / 8.64e+7);
-
-message.channel.send("It has been" +numOfDays+ "days since Thatcher has died");
-Channel.send("Here's a picture of Thatcher's grave", {files: ["https://upload.wikimedia.org/wikipedia/commons/e/e3/MTgrave2.jpeg"]});
+if (args[0] == "--thatcher") {
+    numOfDays = Math.floor(difference / 8.64e+7);
+    message.channel.send("It has been" +numOfDays+ "days since Thatcher has died");
+    Channel.send("Here's a picture of Thatcher's grave", {files: ["https://upload.wikimedia.org/wikipedia/commons/e/e3/MTgrave2.jpeg"]});
 }
     },
 };
